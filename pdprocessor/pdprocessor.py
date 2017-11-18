fbdfcfd092515b82f0f3137eb7b114fde0f06591
Jason Job <jasonwadejob@gmail.com>
2017-11-18 15:40:17 -0800

    Add Path and PDProcessor classes.

--- pdprocessor/pdprocessor.py
+++ pdprocessor/pdprocessor.py
@@ -1,7 +1,40 @@
-
+import os
 
 class PDProcessorError(Exception):
     """A PDProcessor Error."""
 
     def __init__(self, message):
         self.message = message
+
+class Path(object):
+    """A path class."""
+
+    def __init__(self, path_to_file):
+        self.path = path_to_file
+
+
+
+
+    
+
+
+class PDProcessor(Path):
+    """Base class for a pandas dataframe processor.
+
+    A PDProcessor creates a dataframe from a delimited file such as csv or
+    excel. The dataframe is manipulated by adding or formatting columns.
+    """
+
+    def process(self):
+        """Process the file."""
+
+        self.validate_path()
+
+    def validate_path(self):
+        """Validate the path point to a file."""
+
+        if not os.path.isfile(self.path):
+            message = "No file found at '{path}'.".format(path=self.path)
+            raise PDProcessorError(message)
+
+        
--- test/test_pdprocessor.py
+++ test/test_pdprocessor.py
@@ -1,31 +1,51 @@
 """
 Tests for `pdprocessor` module.
 """
+import os
 import pytest
-from pdprocessor.pdprocessor import PDProcessorError
+from pdprocessor.pdprocessor import PDProcessorError, Path, PDProcessor
 
 
 class TestPDProcessorError(object):
 
-    error = PDProcessorError("Error!")
-    try:
-        raise(error)
-    except PDProcessorError as e:
-        pass
-    assert e.message == "Error!"
-        
+    def test_error_message(self):
+        error = PDProcessorError("Error!")
+        try:
+            raise(error)
+        except PDProcessorError as e:
+            pass
+        assert e.message == "Error!"
+
+
+class TestPath(object):
+
+    def test__init__(self):
+        sfile = os.path.join(os.path.dirname(__file__), __file__)
+        path = Path(sfile)
+        assert path.path == sfile
+
+
+class TestPDProcessor(object):
+
+    def test__init__(self):
+        sfile = os.path.join(os.path.dirname(__file__), __file__)
+        processor = PDProcessor(sfile)
+        assert processor.path == sfile
+
+    def test_validate_path_with_valid_file(self):
+        sfile = os.path.join(os.path.dirname(__file__), __file__)
+        processor = PDProcessor(sfile)
+        processor.validate_path()
+
+    def test_validate_path_with_invalid_path(self):
+        sfile = 'invalid_path'
+        processor = PDProcessor(sfile)
+        try:
+            processor.validate_path()
+        except PDProcessorError as e:
+            pass
+        assert isinstance(e, PDProcessorError)
+        assert e.message == "No file found at 'invalid_path'."
 
 
 
-class TestPdprocessor(object):
-
-    @classmethod
-    def setup_class(cls):
-        pass
-
-    def test_something(self):
-        pass
-
-    @classmethod
-    def teardown_class(cls):
-        pass
