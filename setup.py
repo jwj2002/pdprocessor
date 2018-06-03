eaaa86894513f20e2eaaaae71bddcadbb6d3309f
Jason Job <jasonwadejob@gmail.com>
2018-06-03 11:29:14 -0700

    Bumped version to 0.1.1.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='pdprocessor',
-    version='0.1.0',
+    version='0.1.1',
     description='A project that uses pandas to process delimited files.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Jason Job',
